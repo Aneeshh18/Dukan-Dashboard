4fba5b1f13a23704636076b0eb801ca16360ed35
Aneesh Sahu <88325646+Aneeshh18@users.noreply.github.com>
2024-01-09 22:06:14 +0530

    Complete

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import './App.css';
+import Sidebar from './components/Sidebar/Sidebar';
+import Topbar from './components/Topbar/Topbar';
+import Overview from './components/Overview/Overview';
+
+function App() {
+  const [isOpen, setIsOpen] = useState(false)
+  const handleToggleMenu = () => setIsOpen(prev => !prev)
+
+  return (
+    <section className='w-full min-h-screen flex-end'>
+      <Sidebar isOpen={isOpen} handleToggle={handleToggleMenu} />
+
+      <div className=" min-h-screen gap-8 pb-8 flex-col-start bg-[#fafafa] lg:w-[calc(100%-224px)] w-full">
+        <Topbar isOpen={isOpen} handleToggle={handleToggleMenu} />
+        <Overview />
+      </div>
+
+    </section>
+  )
+}
+
+export default App;
